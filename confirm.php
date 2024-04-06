4f89648b8cc7ab5af667b807e2340e174f7a9a0e
ExpReg3 <78870819+ExpReg3@users.noreply.github.com>
2024-04-06 14:34:49 +0200

    PHP: additional info, XSS
    Time stamp + remote address added to the log file, XSS protection filter added

--- confirm.php
+++ confirm.php
@@ -3,8 +3,16 @@
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$postData = file_get_contents('php://input');
 
-	$filename = "data.txt";
+	// anti XSS protection
+	$postData = htmlspecialchars($postData, ENT_QUOTES, "UTF-8");
 
+	$date = date("Y-m-d G:i:s");
+	$inc_ip_address = $_SERVER['REMOTE_ADDR'];
+
+	$postData = $postData . ";" . $date . ";" . $inc_ip_address;
+
+	$filename = "data.txt";
+	
 	$file = fopen($filename, 'a');
 
 	if($file){
